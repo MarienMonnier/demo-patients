fae8fec6c37e2c1ccdb9b2d462cf9ab24282eebd
unknown <r.lesieur@CLT-4.lan.clt-services.com>
2014-12-11 11:25:24 +0100

    Rajout de tests

--- DemoPatients.Tests/ControllerTests.cs
+++ DemoPatients.Tests/ControllerTests.cs
@@ -14,66 +14,15 @@ namespace DemoPatients.Tests
     {
         private PatientController _controller;
         private Mock<IPatientRepository> _repo;
+        private Patient _patient;
 
+        // Initialize : Est appelé avant chaque appel à une méthode de test
         [TestInitialize]
         public void Init()
         {
-            _repo = new Mock<IPatientRepository>();
+            _repo = new Mock<IPatientRepository>(); // <-- Dummy
             _controller = new PatientController(_repo.Object);
-        }
-
-        [TestMethod]
-        public void Controller_Can_CreateOKRedirectToIndex()
-        {
-            var patient = GetPatient();
-
-            var redirect = _controller.Create(new PatientViewModel(patient)) as RedirectToRouteResult;
-
-            if (redirect != null) Assert.AreEqual("Index", redirect.RouteValues["action"]);
-            else Assert.Fail("redirect n'est pas au format RedirectToRouteResult");
-        }
-
-        [TestMethod]
-        public void Controller_Can_CreateKORedirectToEdit()
-        {
-            _repo.Setup(r => r.AddPatient(It.IsAny<Patient>())).Throws(new Exception());
-
-            var patient = GetPatient();
-
-            var redirect = _controller.Create(new PatientViewModel(patient)) as ViewResult;
-
-            if (redirect != null) Assert.AreEqual("Create", redirect.ViewName);
-            else Assert.Fail("redirect n'est pas au format ViewResult");
-        }
-
-        [TestMethod]
-        public void Controller_Can_EditOKRedirectToViewEdit()
-        {
-            var p = GetPatient();
-            var vm = new PatientViewModel(p);
-
-            _repo.Setup(r => r.GetPatientById(It.IsAny<int>())).Returns(p);
-
-            var view = _controller.Edit(5) as ViewResult;
-
-            if (view != null) Assert.AreEqual(vm.Nom, ((PatientViewModel)view.Model).Nom);
-            else Assert.Fail("redirect n'est pas au format ViewResult");
-        }
-
-        [TestMethod]
-        public void Controller_Can_EditNotFoundRedirectToIndex()
-        {
-            _repo.Setup(r => r.GetPatientById(It.IsAny<int>())).Returns(null as Patient);
-
-            var route = _controller.Edit(5) as RedirectToRouteResult;
-
-            if (route != null) Assert.AreEqual("Index", route.RouteValues["action"]);
-            else Assert.Fail("redirect n'est pas au format RedirectToRouteResult");
-        }
-
-        private static Patient GetPatient()
-        {
-            var patient = new Patient()
+            _patient = new Patient()
             {
                 Civilite = Civilite.M,
                 DateNaissance = DateTime.Now,
@@ -81,7 +30,84 @@ namespace DemoPatients.Tests
                 Prenom = "TestP",
                 Present = false
             };
-            return patient;
+        }
+
+        // CleanUp : Est appelé après chaque appel à une méthode de test
+        [TestCleanup]
+        public void Clean()
+        {
+            
+        }
+
+        [TestMethod, Ignore]
+        public void Controller_Can_CreateOKOnRepositoryNull()
+        {
+            // Initialisation
+            var controller = new PatientController(null);
+
+            // Exécution
+            var redirect = controller.Create(new PatientViewModel(_patient)) as RedirectToRouteResult;
+
+            // Vérification
+            if (redirect != null) Assert.AreEqual("Index", redirect.RouteValues["action"]);
+            else Assert.Fail("redirect n'est pas au format RedirectToRouteResult");
+        }
+
+        [TestMethod]
+        public void Controller_Can_CreateOKRedirectToIndex()
+        {
+            // Initialisation
+
+            // Exécution
+            var redirect = _controller.Create(new PatientViewModel(_patient)) as RedirectToRouteResult;
+
+            // Vérification
+            if (redirect != null) Assert.AreEqual("Index", redirect.RouteValues["action"]);
+            else Assert.Fail("redirect n'est pas au format RedirectToRouteResult");
+        }
+
+        [TestMethod]
+        public void Controller_Can_CreateKORedirectToEdit()
+        {
+            // Initialisation
+            _repo.Setup(r => r.AddPatient(It.IsAny<Patient>())).Throws(new Exception()); // <-- Stub
+
+            // Exécution
+            var redirect = _controller.Create(new PatientViewModel(_patient)) as ViewResult;
+
+            // Vérification
+            if (redirect != null) Assert.AreEqual("Create", redirect.ViewName);
+            else Assert.Fail("redirect n'est pas au format ViewResult");
+        }
+
+        [TestMethod]
+        public void Controller_Can_EditOKRedirectToViewEdit()
+        {
+            // Initialisation
+            var vm = new PatientViewModel(_patient);
+
+            _repo.Setup(r => r.GetPatientById(It.IsAny<int>())).Returns(_patient);  // <-- Stub
+
+            // Exécution
+            var view = _controller.Edit(5) as ViewResult;
+
+            // Vérification
+            if (view != null) Assert.AreEqual(vm.Nom, ((PatientViewModel)view.Model).Nom);
+            else Assert.Fail("redirect n'est pas au format ViewResult");
+        }
+
+        [TestMethod]
+        public void Controller_Can_EditNotFoundRedirectToIndex()
+        {
+            // Initialisation
+            _repo.Setup(r => r.GetPatientById(It.IsAny<int>())).Returns(null as Patient);
+
+            // Exécution
+            var route = _controller.Edit(5) as RedirectToRouteResult;
+
+            // Vérification
+            if (route != null) Assert.AreEqual("Index", route.RouteValues["action"]);
+            else Assert.Fail("redirect n'est pas au format RedirectToRouteResult");
         }
     }
 }
