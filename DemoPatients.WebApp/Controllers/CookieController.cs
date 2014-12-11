409cd658e8bc03ac321d6afa45b29f7cd3acd411
Marien Monnier <m.monnier@softit.fr>
2014-12-11 14:41:41 +0100

    Utilisation de la Session

---
+++ DemoPatients.WebApp/Controllers/CookieController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace DemoPatients.WebApp.Controllers
+{
+    public class CookieController : Controller
+    {
+        [HttpPost]
+        public ContentResult HideMessage()
+        {
+            Session.Add("HasHiddenMessage", true);
+            return Content("OK");
+        }
+    }
+}
